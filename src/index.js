18c522f31115e6def19aeba3cb8c684a7ff229d8
Tomas <tomasp1189@gmail.com>
2017-07-14 15:31:01 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/PostsIndex';
+import PostsNew from './components/PostsNew';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={PostsIndex} />
+        {/*<Route path="/posts/:id" component={PostsShow} />*/}
+        <Route path="/posts/new" component={PostsNew} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
